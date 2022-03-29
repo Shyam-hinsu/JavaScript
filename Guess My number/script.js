72a713f7050132ee367980b8936b519d9e6b6901
shyam <shyamhinsu000@gmail.com>
2022-03-29 14:15:23 +0530

    added Guess My number

---
+++ Guess My number/script.js
@@ -0,0 +1,7 @@
+let score = document.querySelector(".score").textContent;
+console.log(score);
+score = 89;
+
+let highscore = (document.querySelector(".highscore").textContent = 0909900);
+
+console.log(highscore);
